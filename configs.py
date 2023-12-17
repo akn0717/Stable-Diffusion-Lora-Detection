a5758d5014e9a062a89cb2805d17899f84ad4b98
Khoa <anhkhoan@sfu.ca>
2023-12-16 18:04:11 -0800

    Update configs

--- configs.py
+++ configs.py
@@ -1,5 +1,5 @@
 BASE_DIR = "."
 OUTPUT_DIR = "{}/output".format(BASE_DIR)
 PRETRAINED_MODEL_URL = "COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml"
-TRAIN_IMAGE_PATH = "dataset/task_anime-2023_11_26_05_06_34-coco 1.0"
-TRAIN_COCO_JSON_PATH = "dataset/task_anime-2023_11_26_05_06_34-coco 1.0"
+TRAIN_IMAGE_PATH = "dataset/lora_detection/images"
+TRAIN_COCO_JSON_PATH = "dataset/lora_detection/annotations/instances_Train.json"
